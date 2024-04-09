777c1548be10cbc0ca3aee3b95ab6b64d2ac9fe7
leb0t <166515731+leb0t@users.noreply.github.com>
2024-04-09 21:25:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/send-discord-message', methods=['POST'])
 def send_message():
   # Replace with your actual webhook URL and message content
-  webhook_url = "YOUR_WEBHOOK_URL"
+  webhook_url = "https://discord.com/api/webhooks/1227323485001617508/2qwsvge6R-RXL1xxmjxNI65xPKPwMVcb0CuLBQQFVL1-uMJ0-37NLOydgqWsiHdn2ENa"
   message = "Hey! Someone just got rickrolled!"
 
   # Use a library like `requests` to send the POST request to Discord
